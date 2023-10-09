406b0de5e057a8c2f1da52441fa993e003284a60
branmkim <bran.mkim@gmail.com>
2023-10-09 01:09:39 -0400

    its minsoo time

---
+++ pages/api/minsoo_api.js
@@ -0,0 +1,32 @@
+import { google } from 'googleapis'
+
+export default async function handler(req, res) {
+    const query = []
+    Object.keys(req.body).forEach((key, index) => {
+        if(key != 'card') query.push(req.body[key])
+    })
+    console.log(query)
+
+    const auth = await google.auth.getClient({
+        projectId: "norse-carport-392418",
+        credentials: {
+            client_email: process.env.CLIENT_EMAIL,
+            private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
+        },
+        scopes: ['https://www.googleapis.com/auth/spreadsheets']
+    })
+    const sheets = google.sheets({ version: 'v4', auth })
+    const body = {
+        values: [
+            query
+        ]
+    }
+    const response = await sheets.spreadsheets.values.append({
+        spreadsheetId: process.env.MINSOO_SHEET_ID,
+        range: 'oct 23' + "!A:C",
+        valueInputOption: "USER_ENTERED",
+        resource: body
+    })
+    console.log(response.status)
+    res.status(200).json({ data: response.data })
+}
---
+++ pages/minsoo/index.js
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+
+export default function Home() {
+    const [toast, setToast] = useState(0)
+
+    const handleClick = async (e) => {
+        e.preventDefault()
+
+        document.querySelector('#submit').disabled = true;
+        
+        var form = document.getElementById("form");
+        var elements = form.elements;
+        for (var i = 0, len = elements.length; i < len; ++i) {
+            elements[i].readOnly = true;
+        }
+        
+        const date = document.querySelector('#date').value
+        const note = document.querySelector('#note').value
+        const amount = document.querySelector('#amount').value
+        
+        const apiResponse = await fetch("/api/minsoo_api", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                date,
+                note,
+                amount
+            })
+        });
+        console.log(apiResponse.status)
+        
+        document.querySelector('#amount').value = ''
+        document.querySelector('#note').value = ''
+        for (var i = 0, len = elements.length; i < len; ++i) {
+            elements[i].readOnly = false;
+        }
+        
+        document.querySelector('#submit').disabled = false;
+
+        if(apiResponse.status == 200) {
+            setToast(1)
+        } else {
+            setToast(2)
+        }
+        setTimeout(() => {
+            setToast(0)
+        }, 1000)
+    }
+
+    return (
+        <main className="px-8 pt-8">
+                <form id="form" onSubmit={handleClick} className='flex flex-col w-60%'>
+                <div className='flex flex-wrap justify-between gap-2 my-1'>
+                    <div className='flex flex-col flex-auto w-36'>
+                        <label className='text-xs'>
+                            Date
+                        </label>
+                        <input id="date" type="date" name="date" placeholder="Date" className='w-full h-10 border-2 rounded-md p-2 text-base'/>
+                    </div>
+
+                    <div className='flex flex-col flex-auto w-36'>
+                        <label className='text-xs'>
+                            Amount
+                        </label>
+                        <input id="amount" type="number" step="0.01" name="amount" placeholder="$ x.xx" className='w-full h-10 border-2 rounded-md p-2 text-base'/>
+                    </div>
+                </div>
+
+                <div className='flex flex-col flex-1 my-1'>
+                    <label className='block text-xs'>
+                        Note
+                    </label>
+                    <input id="note" type="text" name="note" placeholder="Describe transaction" className='w-full border-2 rounded-md p-2 text-base'/>
+                </div>
+                <button id="submit" type="submit" className={' bg-green-400 hover:bg-green-500 disabled:bg-gray-400 transition-colors flex-auto h-10 rounded-md p-1 my-4'}>Submit</button>
+                <div>
+                    {toast == 1 ? <p className='flex absolute bottom-1 right-1 items-center bg-green-400 w-32 h-10 rounded-md p-4 my-1'>Success</p> : null}
+                    {toast == 2 ? <p className='flex absolute bottom-1 right-1 items-center bg-red-400 w-32 h-10 rounded-md p-4 my-1'>Failed</p> : null}
+                </div>
+            </form>
+            
+        </main>
+    )
+}
